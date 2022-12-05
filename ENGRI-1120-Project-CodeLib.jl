710e144613796e1d4946971769506358a9959178
sp2229 <113201385+sp2229@users.noreply.github.com>
2022-12-05 10:09:50 -0500

    Add files via upload

---
+++ ENGRI-1120-Project-CodeLib.jl
@@ -0,0 +1,6 @@
+# my codes -
+include(joinpath("src","Types.jl"))
+include(joinpath("src","Stoichiometric.jl"))
+include(joinpath("src", "Parser.jl"))
+include(joinpath("src","Flux.jl"))
+include(joinpath("src","MSU.jl"))
